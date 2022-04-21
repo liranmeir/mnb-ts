bdd606bf915aa079433eb1f978396d0f112c2c0e
liranmeir <liranmeir@gmail.com>
2022-04-22 00:39:16 +0300

    initial work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,3 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<main class="bg-blue h-screen flex justify-center px-8 py-8">
+	<h1 class="font-bold text-yellow text-8xl">MNB</h1>
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		colors: {
+			blue: '#012F49',
+			yellow: '#EAE1B8'
+		}
 	},
 	plugins: []
 };
