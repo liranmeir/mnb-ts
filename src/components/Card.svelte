52ed43a9780aa5b6a1c41efa28f6fc9be9e7604a
liranmeir <liranmeir@gmail.com>
2022-05-21 21:15:07 +0300

    layout

---
+++ src/components/Card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let url: string;
+</script>
+
+<div class="rounded-md shadow-md bg-yellow px-2 py-2">
+	<iframe
+		src={url}
+		width="100%"
+		height="100%"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	/>
+	<div class="h-10" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,8 @@
 <script>
+	import Card from '../components/Card.svelte';
+	import Layout from './__layout.svelte';
 	const vid = {
-		title: 'Everytime You Go Away',
-		artist: 'Paul Young',
-		thumbnail: 'https://i.ytimg.com/vi/nfk6sCzRTbM/hqdefault.jpg',
-		url: 'https://www.youtube.com/embed/trggwXMMsYw?autoplay=0',
-		duration: '03:00',
-		genre: 'Pop',
-		year: '2019',
-		rating: '4.5',
-		description:
-			'Everytime You Go Away is a song by American singer-songwriter Paul Young. The song was released on December 12, 2019, through his own label, Epic Records.'
+		url: 'https://www.youtube.com/embed/trggwXMMsYw?autoplay=0'
 	};
 	const videos = new Array(10).fill(vid);
 
@@ -26,7 +19,7 @@
 
 <header
 	class:-translate-y-4={y > 64}
-	class="-mt-28 transition h-28 grid grid-flow-col items-center justify-between px-8 gap-8 sticky top-4 z-30 bg-white transform transition duration-500"
+	class="-mt-28 h-28 grid grid-flow-col items-center justify-between px-8 gap-8 sticky top-4 z-30 bg-white transform transition duration-500"
 >
 	<a href="/picker">Player Picker</a>
 
@@ -36,21 +29,6 @@
 <div />
 <div class="grid gap-8 overflow-y-scroll bg-blue px-4">
 	{#each videos as video}
-		<div class="aspect-w-16 aspect-h-9 rounded-md shadow-lg bg-yellow">
-			<iframe
-				src={video.url}
-				width="90%"
-				height="90%"
-				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen
-			/>
-		</div>
+		<Card {...video} />
 	{/each}
 </div>
-
-<style>
-	.shrink {
-	}
-</style>
